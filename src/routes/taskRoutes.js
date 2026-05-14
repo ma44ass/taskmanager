9bb6f3b24db5852c311e4635cba9296d7c72895d
eLecTaha <thoelectrot@gmail.com>
2026-05-14 14:43:53 +0100

    feat: API method post

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -5,5 +5,6 @@ const taskController = require('../controllers/taskController');
 
 // READ : Voir toutes les tâches
 router.get('/', taskController.getAllTasks);
-
+// CREATE : Ajouter une tâche
+router.post('/', taskController.createTask);
 module.exports = router;
