4ef847ef4aa88de08c1b534074c48d1cdb1fd846
ass44ma <assma.boutezrout@gmail.com>
2026-05-14 20:44:10 +0100

    feat: created api delete and update methods

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -25,3 +25,41 @@ exports.getAllTasks = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+
+// --- UPDATE : Modifier une tâche ---
+exports.updateTask = async (req, res) => {
+    try {
+        const { id } = req.params; // On récupère l'ID dans l'URL (ex: /api/tasks/5)
+        const { title, description, status } = req.body;
+        
+        const [result] = await db.query(
+            'UPDATE tasks SET title = ?, description = ?, status = ? WHERE id = ?',
+            [title, description, status, id]
+        );
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Tâche non trouvée" });
+        }
+
+        res.json({ message: "Tâche mise à jour avec succès !" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// --- DELETE : Supprimer une tâche ---
+exports.deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [result] = await db.query('DELETE FROM tasks WHERE id = ?', [id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Tâche non trouvée" });
+        }
+
+        res.json({ message: "Tâche supprimée définitivement !" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -5,6 +5,16 @@ const taskController = require('../controllers/taskController');
 
 // READ : Voir toutes les tâches
 router.get('/', taskController.getAllTasks);
+
 // CREATE : Ajouter une tâche
 router.post('/', taskController.createTask);
+
+ // UPDATE : Modifier une tache
+router.put('/:id', taskController.updateTask); 
+
+ // DELETE : Supprimer une tache
+router.delete('/:id', taskController.deleteTask)
+
+
 module.exports = router;
+
